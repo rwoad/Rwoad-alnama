4c73a44c50deaa98f163c14a8e58b65e4894338a
rwoad <122698987+rwoad@users.noreply.github.com>
2023-01-18 12:31:54 +0300

    Add files via upload

--- saudi_einvoice_knk/__manifest__.py
+++ saudi_einvoice_knk/__manifest__.py
@@ -27,7 +27,7 @@
     'author': 'Kanak Infosystems LLP.',
     'website': 'https://www.kanakinfosystems.com',
     'license': 'OPL-1',
-    'depends': ['account','l10n_sa'],
+    'depends': ['account'],
     'data': [
         'report/vat_invoice_report_print.xml',
         'report/vat_report_action_call.xml',
